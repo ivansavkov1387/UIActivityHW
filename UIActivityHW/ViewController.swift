ca04b421193025cf33b340dd57f2a59b7ff97bed
Ivan Savkov <ivansavkov1387@gmail.com>
2021-04-30 12:27:24 +0300

    Initial Commit

---
+++ UIActivityHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIActivityHW
+//
+//  Created by Иван on 4/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
